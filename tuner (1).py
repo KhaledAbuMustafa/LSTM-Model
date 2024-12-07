5701ed44a5ccae7ce496db81966918a4bb6738f7
KhaledAbuMustafa <khaled.abu-mustafa@web.de>
2024-12-07 12:13:06 +0100

    Update tuner (1).py

--- tuner (1).py
+++ tuner (1).py
@@ -7,9 +7,11 @@ import keras_tuner as kt
 class Tuner:
     def build_model(self, hp):
         model = tf.keras.Sequential()
+
+        # Input layer
         model.add(layers.Input((32, 1)))
 
-        
+        # Add LSTM layers with a tunable number of layers and units
         num_lstm_layers = hp.Int('num_lstm_layers', 1, 4)
         for i in range(num_lstm_layers):
             model.add(layers.LSTM(
@@ -18,16 +20,16 @@ class Tuner:
                 kernel_regularizer=regularizers.l2(1e-6)
             ))
 
-        
+        # Add Dense layers with tunable units and number of layers
         num_dense_layers = hp.Int('num_dense_layers', 1, 3)
         for i in range(num_dense_layers):
             model.add(layers.Dense(
                 units=hp.Int(f'dense_units_{i}', min_value=16, max_value=128, step=16),
                 activation='relu'
             ))
-
+        # Add a Dropout layer with a tunable dropout rate
         model.add(layers.Dropout(hp.Float('dropout', min_value=0.0, max_value=0.5, step=0.1)))
-        model.add(layers.Dense(1))
+        model.add(layers.Dense(1)) # Output layer
 
         model.compile(
             loss='mean_absolute_error',
